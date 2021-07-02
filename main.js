4a48a92e55ed3465ab1c096cbe35c69b47af53ff
Beka00best <bukhatovb@mail.ru>
2021-07-02 17:47:35 +0600

    add crash logic and score

--- main.js
+++ main.js
@@ -44,6 +44,18 @@ function moveRoad() {
 function moveEnemy() {
     let enemy = document.querySelectorAll('.enemy');
     enemy.forEach(function(item){
+        let carRect = car.getBoundingClientRect(); //
+        let enemyRect = item.getBoundingClientRect();
+
+        if (carRect.top <= enemyRect.bottom &&
+            carRect.right >= enemyRect.left &&
+            carRect.left <= enemyRect.right &&
+            carRect.bottom >= enemyRect.top) {
+                setting.start = false;
+                start.classList.remove('hide');
+                start.style.top = score.offsetHeight;
+        }
+
         item.y += setting.speed / 2;
         item.style.top = item.y + 'px';
         if (item.y >= document.documentElement.clientHeight) {
@@ -56,6 +68,8 @@ function moveEnemy() {
 
 function playGame(){
     if (setting.start){
+        setting.score += setting.speed;
+        score.innerHTML = 'Score<br>' + setting.score;
         moveRoad();
         moveEnemy();
         if (keys.ArrowLeft && setting.x > 0) {
@@ -88,9 +102,11 @@ const getRandomEnemy = (max) => Math.floor((Math.random() * max) + 1);
 
 function startGame(){
     // document.body.append(music);
+
     music.play();
 
     start.classList.add('hide');
+    gameArea.innerHTML = '';
 
     for (let i = 0; i < getQuantityElements(100); i++) {
         const line = document.createElement('div');
@@ -113,9 +129,12 @@ function startGame(){
             no-repeat`;
         gameArea.appendChild(enemy);
     }
-
+    setting.score = 0;
     setting.start = true;
     gameArea.appendChild(car);
+    car.style.left = gameArea.offsetWidth / 2 - car.offsetWidth / 2;
+    car.style.top = 'auto';
+    car.style.bottom = '10px';
     setting.x = car.offsetLeft;
     setting.y = car.offsetTop;
     requestAnimationFrame(playGame);
